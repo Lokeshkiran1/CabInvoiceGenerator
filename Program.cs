4fcbda5ae388560aec8393e047aba1350e45c6ba
LokeshKiran <lokeshkskiran@gmail.com>
2022-10-06 22:18:32 +0530

    [Lokesh]Added:Given distance and time,the invoice generator should return the total fare for the journey

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CabInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator Application");
+            //InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            //double fair = invoiceGenerator.CalculateFare(2, 5);
+            //Console.WriteLine("total fair for the ride:"+fair);
+        }
+    }
+}
